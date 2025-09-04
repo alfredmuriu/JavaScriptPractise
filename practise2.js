dceedf41c393c35a008c79f3b04a20f43155527a
Alfred Muriu <alfredmuriu19@gmail.com>
2025-09-04 08:09:09 +0300

    Add files via upload

---
+++ practise2.js
@@ -0,0 +1,10 @@
+let username = "Joe"
+let message = "You have three new notifications"
+
+let messageToUser = username +" "+ message
+
+let myName = "My name is Alfred"
+let greetings = "Hello,"
+
+let myGreeting = greetings +"  "+ myName
+console.log(myGreeting)
